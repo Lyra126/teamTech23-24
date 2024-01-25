a09bc2cd27b6decabcd738241b0ee0a8b6b2bfc4
clynae2 <48706019+Lyra126@users.noreply.github.com>
2024-01-25 18:44:34 -0500

    Update Satellite.h

--- teamTech23-24-Backend/ReadData/Satellite.h
+++ teamTech23-24-Backend/ReadData/Satellite.h
@@ -17,82 +17,90 @@
 using namespace std;
 
 class Satellite {
-private:
-    libsgp4::Tle tle;
+    private:
+        libsgp4::Tle tle;
 
-    //duration that the satellite will communicate with the ground station
-    // vector of start time end time pairs
-    std::vector<std::pair<libsgp4::DateTime, libsgp4::DateTime>> passes;
+        //duration that the satellite will communicate with the ground station
+        // vector of start time end time pairs
+        std::vector<std::pair<libsgp4::DateTime, libsgp4::DateTime>> passes;
 
-    libsgp4::DateTime startTime;
-    libsgp4::DateTime endTime;
-    libsgp4::DateTime dt;
-    libsgp4::Eci eci;
-    libsgp4::CoordTopocentric topo;
-    libsgp4::CoordGeodetic geo;
-    libsgp4::Observer obs;
-    libsgp4::SGP4 sgp4;
-    double maxElevation;
-    int rank;
-    bool constructorSuccess;
+        libsgp4::DateTime startTime;
+        libsgp4::DateTime endTime;
+        libsgp4::DateTime dt;
+        libsgp4::Eci eci;
+        libsgp4::CoordTopocentric topo;
+        libsgp4::CoordGeodetic geo;
+        libsgp4::Observer obs;
+        libsgp4::SGP4 sgp4;
+        double maxElevation;
+        int rank;
+        bool constructorSuccess;
 
-    int passNumber;
+        int passNumber;
 
-    // For the database
-    std::string name;
-    std::string startString;
-    std::string endString;
+        // For the database
+        std::string name;
+        std::string startString;
+        std::string endString;
 
-public:
-    //constructor
-    Satellite(libsgp4::Tle tle1, libsgp4::DateTime currentTime): tle(tle1), dt(currentTime), eci(dt, 1, 1, 1), obs(29,82,1), sgp4(tle){
-        constructorSuccess = true;
-        maxElevation = 0;
-        passNumber = -1;
-        try{
-            eci = sgp4.FindPosition(dt);
-            geo = eci.ToGeodetic();
-        } catch(libsgp4::SatelliteException& e){
-            std::cout << "Could not calculate ECI" << std::endl;
-            constructorSuccess = false;
+    public:
+        //constructor
+        Satellite(libsgp4::Tle tle1, libsgp4::DateTime currentTime): tle(tle1), dt(currentTime), eci(dt, 1, 1, 1), obs(29,82,1), sgp4(tle){
+            constructorSuccess = true;
+            maxElevation = 0;
+            passNumber = -1;
+            try{
+                eci = sgp4.FindPosition(dt);
+                geo = eci.ToGeodetic();
+            } catch(libsgp4::SatelliteException& e){
+                std::cout << "Could not calculate ECI" << std::endl;
+                constructorSuccess = false;
+            }
+
         }
 
-    }
 
+        bool isLEO();
 
-    bool isLEO();
+        void calculatePos(float timePassed);
 
-    void calculatePos(float timePassed);
+        float calculateDistance();
 
-    float calculateDistance();
+        void assignRank();
 
-    void assignRank();
+        void generatePasses();
 
-    void generatePasses();
+        int getNumPasses();
 
-    int getNumPasses();
+        bool getConstrSuccess();
 
-    bool getConstrSuccess();
+        string getName();
 
-    string getName();
+        string getStartString();
 
-    string getStartString();
+        string getEndString();
 
-    string getEndString();
+        libsgp4::DateTime getStartTime();
 
-    libsgp4::DateTime getStartTime();
+        libsgp4::DateTime getEndTime();
 
-    libsgp4::DateTime getEndTime();
+        float getStartTimeLatitude();
 
-    bool setStartAndEndTime();
+        float getStartTimeLongitude();
 
-    void toString();
+        float getEndTimeLatitude();
 
-    int getRank();
+        float getEndTimeLongitude();
 
-    libsgp4::DateTime getEarliestEndTime() const;
+        bool setStartAndEndTime();
 
-    // Overload < for Min Heap
-    bool operator< (const Satellite& rSat) const;
+        void toString();
+
+        int getRank();
+
+        libsgp4::DateTime getEarliestEndTime() const;
+
+        // Overload < for Min Heap
+        bool operator< (const Satellite& rSat) const;
 };
 
