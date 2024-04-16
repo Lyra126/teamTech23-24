96825c81631e3a4edde6c020f4de15ad67494212
Clarissa Cheung <55814603+lynae126@users.noreply.github.com>
2024-04-16 12:22:41 -0400

    backend team 1 finished the algorhtm

--- teamTech23-24-Backend/ReadData/Satellite.h
+++ teamTech23-24-Backend/ReadData/Satellite.h
@@ -3,7 +3,6 @@
 //
 
 #pragma once
-
 #include <utility>
 #include <vector>
 #include "libsgp4/Tle.h"
@@ -18,12 +17,9 @@ using namespace std;
 
 class Satellite {
     private:
-        libsgp4::Tle tle;
-
-        //duration that the satellite will communicate with the ground station
-        // vector of start time end time pairs
+        //duration that the satellite will communicate with the ground station vector of start time end time pairs
         std::vector<std::pair<libsgp4::DateTime, libsgp4::DateTime>> passes;
-
+        libsgp4::Tle tle;
         libsgp4::DateTime startTime;
         libsgp4::DateTime endTime;
         libsgp4::DateTime dt;
@@ -32,13 +28,12 @@ class Satellite {
         libsgp4::CoordGeodetic geo;
         libsgp4::Observer obs;
         libsgp4::SGP4 sgp4;
+
         double maxElevation;
         int rank;
         bool constructorSuccess;
-
         int passNumber;
 
-        // For the database
         std::string name;
         std::string startString;
         std::string endString;
@@ -59,48 +54,48 @@ class Satellite {
 
         }
 
+        //accessors
+        string getName() const;
+        int getRank() const;
+        string getStartString() const;
+        string getEndString() const;
+        libsgp4::DateTime getStartTime() const;
+        libsgp4::DateTime getEndTime() const;
+        float getStartTimeLatitude() const;
+        float getStartTimeLongitude() const;
+        float getEndTimeLatitude() const;
+        float getEndTimeLongitude() const;
 
-        bool isLEO();
-
-        void calculatePos(float timePassed);
-
-        float calculateDistance();
-
-        void assignRank();
-
-        void generatePasses();
-
-        int getNumPasses();
-
-        bool getConstrSuccess();
-
-        string getName();
-
-        string getStartString();
-
-        string getEndString();
-
-        libsgp4::DateTime getStartTime();
-
-        libsgp4::DateTime getEndTime();
-
-        float getStartTimeLatitude();
-
-        float getStartTimeLongitude();
-
-        float getEndTimeLatitude();
-
-        float getEndTimeLongitude();
-
-        bool setStartAndEndTime();
-
-        void toString();
-
-        int getRank();
-
+        libsgp4::Tle getTle() const;
+        std::vector<std::pair<libsgp4::DateTime, libsgp4::DateTime>> getPasses() const;
+        libsgp4::DateTime getDt() const;
+        libsgp4::Eci getEci() const;
+        libsgp4::CoordTopocentric getTopo() const;
+        libsgp4::CoordGeodetic getGeo() const;
+        libsgp4::Observer getObs() const;
+        libsgp4::SGP4 getSgp4() const;
+        int getMaxElevation() const;
+        int getPassNumber() const;
+        int getNumPasses() const;
+        bool getConstrSuccess() const;
         libsgp4::DateTime getEarliestEndTime() const;
 
-        // Overload < for Min Heap
+        //mutators
+        bool setStartAndEndTime();
+
+
+        bool isLEO();
+        void calculatePos(float timePassed);
+        float calculateDistance(float gs_lat, float gs_lon, float gs_alt, float x_point, float y_point);
+        void assignRank();
+        void generatePasses();
+        int findShorterDistance(float gs1_lat, float gs1_lon, float gs1_alt, float gs2_lat, float gs2_lon, float gs2_alt, float start_lat, float start_lon, float end_lat, float end_lon);
+
+        // operator overloaders
         bool operator< (const Satellite& rSat) const;
+        Satellite& operator= (const Satellite& rSat);
+        bool operator== (const Satellite& other) const;
+
+        void toString();
 };
 
